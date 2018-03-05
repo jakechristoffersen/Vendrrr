341d6f04d9d5617391ebf3cea9f2f2aaf64b7704
jakechristoffersen <jakec1481@gmail.com>
2018-03-05 08:04:29 -0700

    middle-down

--- vender-controller.js
+++ vender-controller.js
@@ -3,11 +3,58 @@ function VenderController(){
   var venderService = new VenderService()
 
   //items is an array we need to get from the service and give to drawItems
-  function drawItems(items){
+  function drawItems() {
     //we will take in a paramter(items) and iterate over it to build
     //a template to draw to the screen.
+    var items = venderService.getItems()
 
-  }
+    console.log(items)
+
+    var template = `
+    
+    <h3>Vend that Machine</h3>
+
+    
+    <table class="table">
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Amount</th>
+        <th>ID</th>
+      </tr>
+    `;
+
+  
+    for (let i = 0; i < items.length; i++) {
+      var item = items[i];
+      var itemAmount = item.amount;
+      var itemPrice = item.price;
+      console.log(itemAmount, itemPrice)
+
+      template += `
+      
+      <tr>
+        <td id="name">${item.name}</td>
+        <td id="desc">${item.description}</td>
+        <td id="price">${item.price}</td>
+        <td id="amount">${item.amount}</td>
+        <td>${item.amount}</td>
+      </tr>
+
+      `;
+      
+    }
+    
+
+    template += "<table>"
+
+    document.getElementById("items").innerHTML = template
+
+
+}
+
+  drawItems()
 
 
   //public parts
--- vender-service.js
+++ vender-service.js
@@ -1,7 +1,8 @@
 function VenderService(){
   //private parts
   var money = 0
-
+  var input
+  
   //our purchaseable items, add some of your own!
   //we could add an id property to these items to display location for purchasing(A1, D4, etc)
   var items = [
@@ -9,39 +10,65 @@ function VenderService(){
       name: "Fruit Snack Gummies",
       description: "Fruit salad done right.",
       price: 1.00,
-      amount: 45
+      amount: 45,
+      id: "A1"
     },
     {
       name: "Plant Food",
       description: "Are you hungry? I could use a light snack.",
       price: 5.00,
-      amount: 20
+      amount: 20,
+      id: "A2"
     },
     {
       name: "Admiral-able Ack-Bar",
       description: "Snack like an Admiral.",
       price: .75,
-      amount: 3
+      amount: 3,
+      id: "A3"
     },
     {
       name: "Paranormal Snacktivity",
       description: "The perfect midnight snack.",
       price: .25,
-      amount: 7
+      amount: 7,
+      id: "A4"
     },
     {
       name: "The Void",
       description: "This snack eats you!",
       price: 3.25,
-      amount: 1
+      amount: 1,
+      id: "A5"
     }
   ]
+  
+  function Item(name, description, price, amount) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.amount = amount;
+    this.id = id;
+  }
 
+  this.getItems = function() {
+    return items;
+  }
+
+  this.reduceAmount = function() {
+    this.amount - 1
+
+    if (amount >= 0) {
+      amount = 0
+    }
+  }
   //public parts
 
   //add money needs to increase the money amount, and return it to the view.  check if statement to added per click, subtract money on item purchase.
-  this.addMoney = function(){
+  this.addMoney = function(input){
+
     money += .25
+    console.log(money)
   }
 
 
